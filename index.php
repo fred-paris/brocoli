53e486dfab509d4008f3a53ebc2522ccc70068a0
fred-paris <fredem66@gmail.com>
2015-07-17 11:08:27 +0200

    Add main stylesheet

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Brocoli</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
 <body>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, enim aliquam ipsum nesciunt et provident 
-		mollitia. Aliquam, cupiditate, quidem ea sapiente aperiam incidunt temporibus esse blanditiis facilis accusamus nam similique.</p>
+	mollitia. Aliquam, cupiditate, quidem ea sapiente aperiam incidunt temporibus esse blanditiis facilis accusamus nam similique.</p>
 	
 </body>
 </html>

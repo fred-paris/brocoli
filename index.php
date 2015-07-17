a7b603b64fabe2f1c33f725ddc4bbf3af6c65ef5
fred-paris <fredem66@gmail.com>
2015-07-17 10:58:27 +0200

    Add contents and some headers

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr-FR">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Brocoli</title>
 </head>
 <body>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, enim aliquam ipsum nesciunt et provident 
+		mollitia. Aliquam, cupiditate, quidem ea sapiente aperiam incidunt temporibus esse blanditiis facilis accusamus nam similique.</p>
 	
 </body>
 </html>
